b6ae498a295a43435e18ef427541449b2041e8d1
ferdous-fashion <ferdousrahman2504@gmail.com>
2026-07-27 09:09:14 +0600

    Delete robots.ts

--- robots.ts
+++
@@ -1,1 +0,0 @@
-import type { MetadataRoute } from 'next'; export default function robots():MetadataRoute.Robots{return {rules:{userAgent:'*',allow:'/',disallow:'/admin/dashboard'},sitemap:`${process.env.NEXT_PUBLIC_SITE_URL||'http://localhost:3000'}/sitemap.xml`}}
