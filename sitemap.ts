f282c683f9945cbdcbdc28f6a0411fa2d7f670d9
ferdous-fashion <ferdousrahman2504@gmail.com>
2026-07-27 14:27:09 +0600

    Delete sitemap.ts

--- sitemap.ts
+++
@@ -1,1 +0,0 @@
-import type { MetadataRoute } from 'next'; export default function sitemap():MetadataRoute.Sitemap{return [{url:process.env.NEXT_PUBLIC_SITE_URL||'http://localhost:3000',lastModified:new Date(),changeFrequency:'weekly',priority:1}]}
